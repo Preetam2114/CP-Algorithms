6981c572ad9fc8aef5a912812a21180a07a104fc
Preetam2114 <preetamrane2114@gmail.com>
2020-07-25 13:28:58 +0530

    Prime numbers

---
+++ Algebra/Prime numbers/Sieve of Eratosthenes/Sieve of Eratosthenes.cpp
@@ -0,0 +1,20 @@
+/*
+ * File: Sieve of Eratosthenes
+ * Algorithm: Sieve of Eratosthenes
+ * Created Date: Saturday July 25th 2020
+ * Author: preetam rane
+ */
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}
