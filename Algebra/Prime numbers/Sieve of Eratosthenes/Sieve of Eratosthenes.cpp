773ea1a6b78fc8f621022263515735509b1929ef
Preetam2114 <preetamrane2114@gmail.com>
2020-07-27 19:13:06 +0530

    Sieve of Eratosthenes

--- Algebra/Prime numbers/Sieve of Eratosthenes/Sieve of Eratosthenes.cpp
+++ Algebra/Prime numbers/Sieve of Eratosthenes/Sieve of Eratosthenes.cpp
@@ -6,15 +6,41 @@
  */
 
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
+bool Check(int N,int pos){ 
+    return (bool)(N & (1<<pos));
+}
 
+void Set(int &N,int pos){ 
+    N=N | (1<<pos);
+}
+
+const int MAX = 99998953;
+int prime[(MAX>>5)+100];
+
+void sieve(){
+    int lim = sqrt(MAX);
+    for(int i=3; i<=lim; i+=2){
+        if(!Check(prime[i>>5],i&31)){
+            for(int j=i*i; j<=MAX; j+=(i<<1)){
+                Set(prime[j>>5],j&31);
+            }
+        }
     }
-    return 0;
+    cout << 2 << endl;
+    int n = 1;
+    for(int i=3; i<=MAX; i+=2){
+        if(!Check(prime[i>>5],i&31)){
+            n++;
+            if(n%100 == 1)
+                cout << i << endl;
+        }
+    }
+}
+int main()
+{
+    sieve();
 }
