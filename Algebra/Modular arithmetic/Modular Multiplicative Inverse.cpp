56004e12b02db77256f99784335ab6bb554cb734
Preetam2114 <preetamrane2114@gmail.com>
2020-08-03 22:15:54 +0530

    Modular Multiplicative Inverse

---
+++ Algebra/Modular arithmetic/Modular Multiplicative Inverse.cpp
@@ -0,0 +1,23 @@
+/*
+ * File: Modular Multiplicative Inverse
+ * Algorithm: Modular arithmetic
+ * Created Date: Monday August 3rd 2020
+ * Author: preetam rane
+ */
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}
