25bc84ec2be0999b6d0257243b99b1cc4dc6222f
Preetam2114 <preetamrane2114@gmail.com>
2020-08-04 22:08:11 +0530

    Chinese Remainder Theorem

---
+++ Algebra/Modular arithmetic/Chinese Remainder Theorem/Hackerrank - Number of sequences.cpp
@@ -0,0 +1,23 @@
+/*
+ * File: Hackerrank - Number of sequences
+ * Algorithm: Chinese Remainder Theorem
+ * Created Date: Tuesday August 4th 2020
+ * Author: preetam rane
+ */
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}
